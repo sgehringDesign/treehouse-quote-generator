a888d5c45bd9f1b5c4f77620c4ea6d44e9b2cbe6
Stephen Gehring <sgehringdesign@gmail.com>
2016-09-21 23:48:33 -0700

    draft - hook up api

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-var quotes = ""
-
-
+var q = new Array();
 
 function printQuote() {
+  console.log('1 -----------------');
+  $.ajaxSetup({ cache: false });
   
-  console.log(0);
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    console.log(a);
+    q.push(a);
+    console.log(q);
+    console.log('2 -----------------');
+    
+    $('.quote').text( $(a[0].content ).text());
+    $('.source').html(a[0].title);
+    
+  });
   
+  console.log('3 -----------------');
 }
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
