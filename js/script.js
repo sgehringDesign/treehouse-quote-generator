572153afc564e89492c32bc0aa0aa8ab82744721
Stephen Gehring <sgehringdesign@gmail.com>
2016-09-21 20:53:28 -0700

    testing basic js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+var quotes = ""
+
+
+
+function printQuote() {
+  
+  console.log(0);
+  
+}
+
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
