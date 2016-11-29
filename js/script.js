561a06862d006e1055f7e70d13604a84ba24239a
Stephen Gehring <sgehringdesign@gmail.com>
2016-11-29 02:53:46 -0800

    hook up js to api

--- js/script.js
+++ js/script.js
@@ -1,24 +1,44 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-var q = new Array();
 
-function printQuote() {
-  console.log('1 -----------------');
-  $.ajaxSetup({ cache: false });
-  
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-    console.log(a);
-    q.push(a);
-    console.log(q);
-    console.log('2 -----------------');
-    
-    $('.quote').text( $(a[0].content ).text());
-    $('.source').html(a[0].title);
-    
-  });
-  
-  console.log('3 -----------------');
-}
+(function () {
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+  var quotes;
+  var current = 0;
+
+  var loadQuotes = function () {
+
+    $.ajaxSetup({ 
+      cache: false 
+    });
+
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30x&callback=",
+      function(response) {
+        quotes = response;
+
+          $(document).ready(function(){ 
+            $("#quote").html(quotes[current].content);
+            $("#source").html('<a href="'+quotes[current].link+'" class="source" target="_blank" >'+quotes[0].title+'</a>');
+          });
+
+      }
+    );
+
+  }
+
+  var printQuote = function () {
+    current = current + 1; 
+    if(current >= quotes.length) {
+      current = 0;
+    }
+    $("#quote").html(quotes[current].content);
+    $("#source").html('<a href="'+quotes[current].link+'" class="source" target="_blank" >'+quotes[current].title+'</a>');
+    console.log(current);
+  }
+
+  document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+  var quote = loadQuotes();
+
+}());
 
