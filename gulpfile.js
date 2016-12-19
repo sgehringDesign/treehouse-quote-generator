bfc860c740a403bcc7f289fdd30ebee8983eb7df
Stephen Gehring <sgehringdesign@gmail.com>
2016-12-18 21:17:59 -0800

    fixed gulp build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,51 @@
 var gulp = require('gulp'),
     gp_concat = require('gulp-concat'),
-    gp_uglify = require('gulp-uglify');
+    gp_uglify = require('gulp-uglify'),
+    pump = require('pump');
+
+var jsFilesDist = [
+  './bower_components/jquery/dist/jquery.js',
+  './bower_components/jquery/dist/jquery.min.js',
+  './bower_components/jquery/dist/jquery.min.map'
+  //'./bower_components/jquery/dist/*.*'
+];
+
+var jsFilesConcat = [
+  './js/jquery.js',
+  './js/app-dev.js'
+];
 
 
-gulp.task('build', function(){
-  return gulp.src(['./bower_components/jquery/dist/jquery.min.js', './js/script.js'])
-    .pipe(gp_concat('concat.js'))
-    .pipe(gp_uglify())
-    .pipe(gulp.dest('js'));
+gulp.task('move', function(){
+  return gulp.src(jsFilesDist)
+  .pipe(gulp.dest('js'));
+}); 
+
+gulp.task('concat', ['move'], function() {
+  return gulp.src(jsFilesConcat)
+    .pipe(gp_concat('app.js'))
+    .pipe(gulp.dest('js'));  
+  
 });
+
+gulp.task('compress', ['concat'], function (cb) {
+  console.log('1!!!!');
+  
+  pump([ 
+      gulp.src('./js/app.js'),
+      gp_uglify(),
+      gulp.dest('js')
+    ],
+    cb
+  );
+  
+  console.log('2!!!!');
+
+});
+
+gulp.task('build', ['move', 'concat', 'compress'], function(){ 
+  // place extra code for build task here'
+}); 
 
 gulp.task('default', ['build'], function() {
   // place code for your default task here'
